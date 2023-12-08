d45329eef6af1a40756bbeea16fe39e293eb9b49
zkan <zkan.cs@gmail.com>
2023-12-08 10:30:37 +0700

    Explain variables

--- app.rb
+++ app.rb
@@ -1,1 +1,13 @@
 name = "John"
+
+# Global variable
+$redis_conn = Redis.new
+
+# Class variable - ซึ่งจะไม่ค่อยได้ใช้ ไม่แนะนำให้ใช้จาก style guide ต่าง ๆ
+@@count = 1
+
+# Instance variable - ตัวแปรที่อยู่ใน instance ของ class
+@name = "Kan"
+
+# local, block
+country = "Thailand"
