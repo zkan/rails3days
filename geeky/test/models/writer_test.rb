4f17f160da944e443adb14dff4f3dc3ef29b6d50
zkan <zkan.cs@gmail.com>
2023-12-08 14:55:08 +0700

    Add writer test with fixtures

--- geeky/test/models/writer_test.rb
+++ geeky/test/models/writer_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class WriterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @writer = writers(:john)
+  end
+
+  def test_count
+    assert_equal Writer.count, 2
+  end
+
+  def test_name
+    assert_equal @writer.name, "John"
+  end
 end
