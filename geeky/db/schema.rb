de92437e54ed1d132983d5556240fb9ebfc2d3ef
zkan <zkan.cs@gmail.com>
2023-12-08 14:44:15 +0700

    Migrate writer

--- geeky/db/schema.rb
+++ geeky/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_08_074149) do
+  create_table "writers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
