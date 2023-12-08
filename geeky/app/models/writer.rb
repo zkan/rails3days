e5e9ae501bfbc36cf454ac72418e60532548acba
zkan <zkan.cs@gmail.com>
2023-12-08 16:41:07 +0700

    Add dependent destroy when delete writer also delete post

--- geeky/app/models/writer.rb
+++ geeky/app/models/writer.rb
@@ -1,5 +1,8 @@
 class Writer < ApplicationRecord
-  has_many :posts
+  # has_many :posts
+
+  # เป็นการที่เราลบ writer แล้ว ให้ลบ post ทิ้งให้หมดด้วย
+  has_many :posts, dependent: :destroy
 
   # Below is a class method -> Writer.count
   # def self.select_lists
