a80d05a8c289fce32d12a1e33c6647e72ad2d032
zkan <zkan.cs@gmail.com>
2023-12-08 16:04:07 +0700

    Implement select lists in Writer

--- geeky/app/models/writer.rb
+++ geeky/app/models/writer.rb
@@ -1,3 +1,21 @@
 class Writer < ApplicationRecord
   has_many :posts
+
+  # Below is a class method -> Writer.count
+  # def self.select_lists
+  # end
+
+  # Recommend this เพราะว่าเราอาจจะมีหลาย class method จะได้อยู่ใน block เดียวกัน
+  class << self
+    def select_lists
+      # all.map { |writer| [writer.name, writer.id] }
+      all.map do |writer|
+        [writer.name, writer.id]
+      end
+    end
+  end
+
+  # Below is an instance method -> writer.display_name
+  def display_name
+  end
 end
--- geeky/test/models/writer_test.rb
+++ geeky/test/models/writer_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class WriterTest < ActiveSupport::TestCase
   def setup
-    @writer = writers(:john)
+    @john = writers(:john)
+    @kan = writers(:kan)
   end
 
   def test_count
@@ -10,14 +11,19 @@ class WriterTest < ActiveSupport::TestCase
   end
 
   def test_name
-    assert_equal @writer.name, "John"
+    assert_equal @john.name, "John"
   end
 
   def test_age
-    assert_equal @writer.age, 18
+    assert_equal @john.age, 18
   end
 
   def test_has_many_posts
-    assert_equal @writer.posts.count, 1
+    assert_equal @john.posts.count, 1
+  end
+
+  def test_select_lists
+    list = [[@john.name, @john.id], [@kan.name, @kan.id]].sort_by {|name, id| id}
+    assert_equal Writer.select_lists, list
   end
 end
