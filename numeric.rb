ddf4abad15033a1824fc5b97436f0c61d4b77f03
zkan <zkan.cs@gmail.com>
2023-12-08 10:54:29 +0700

    Play with numeric

---
+++ numeric.rb
@@ -0,0 +1,19 @@
+i = 5
+f = 5.0
+
+5 + 5.0
+# => 10.0
+
+(5 + 5.0).round
+
+# Big Decimal
+# BigDecimal("0.0001")
+
+# Rational Number - จำนวนตรรกยะ เช่น 1/2
+r = 5/8r
+
+5/8r + 3/8r
+
+n = 3
+n = n + 5
+n += 1
