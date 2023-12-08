f84087b3b0cbc80ad66db9e48bae88d6b7e087a1
zkan <zkan.cs@gmail.com>
2023-12-08 11:36:35 +0700

    Play with control and loop

---
+++ control.rb
@@ -0,0 +1,33 @@
+# If
+n = 5
+
+if n < 5
+  puts n
+elsif n > 5
+  puts false
+else
+  puts "blank"
+end
+
+case n
+when n < 5
+  puts n
+else
+  puts "blank"
+end
+
+
+# Loop
+(0..5).to_a
+
+for i in 0..5
+  puts i
+end
+
+while true
+  break
+  next
+end
+
+# Falsy
+# nil, false
