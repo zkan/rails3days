1d0057dca7f72f814aa62e36ae573ead5a472acf
zkan <zkan.cs@gmail.com>
2023-12-09 16:41:26 +0700

    Fix samples url

--- geeky/test/controllers/samples_controller_test.rb
+++ geeky/test/controllers/samples_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class SamplesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get samples_show_url
+  test "should get index" do
+    get admin_samples_url
     assert_response :success
   end
 end
