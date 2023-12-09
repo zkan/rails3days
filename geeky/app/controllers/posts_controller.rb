314b47a785c46ca6079d71a602ad619c86149545
zkan <zkan.cs@gmail.com>
2023-12-09 16:15:45 +0700

    Change number of posts per page

--- geeky/app/controllers/posts_controller.rb
+++ geeky/app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  NUMBER_PER_PAGE = 1
+  NUMBER_PER_PAGE = 3
 
   # GET /posts or /posts.json
   def index
