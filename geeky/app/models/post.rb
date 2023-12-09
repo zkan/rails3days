b2a09754427c3ea4d5c7075c6fd1c867a00b34dc
zkan <zkan.cs@gmail.com>
2023-12-09 10:28:15 +0700

    Move model validation to concerns

--- geeky/app/models/post.rb
+++ geeky/app/models/post.rb
@@ -1,17 +1,10 @@
 class Post < ApplicationRecord
-  belongs_to :writer
   validates :body, presence: true
+  belongs_to :writer
 
-  # Custom validation uses "validate" without s
-  validate :body_more_than_10
+  include ContentEditable
 
   def writer_name
     writer.name
-  end
-
-  def body_more_than_10
-    if body.nil? || body.length < 10
-      errors.add(:body, "must have more than 10 characters")
-    end
   end
 end
