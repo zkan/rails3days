e32bbb22fd23aca65dbeed4d720c11f9f43dbfce
zkan <zkan.cs@gmail.com>
2023-12-09 11:50:56 +0700

    Use counter cache

--- geeky/app/models/post.rb
+++ geeky/app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ApplicationRecord
   validates :body, presence: true
-  belongs_to :writer
+
+  # counter_cache column: :posts_count
+  # usage:
+  # reset_counter: Writer.reset_counters(id, :posts_count)
+  belongs_to :writer, counter_cache: true
+  # ใส่ counter_cache จะได้คอลัมน์ใหม่ชื่อ posts_count แล้วต้องรัน migrate ด้วย เวลาที่ใช้ฟีเจอร์นี้
+  # ถ้าอยากเปลี่ยนชื่อ column ให้ใช้แบบนี้
+  # belongs_to :writer, counter_cache: :awesome_posts_count
+
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
