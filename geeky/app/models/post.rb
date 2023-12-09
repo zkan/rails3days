3f82c7ea8f3941ac55980f35651b8b8cfa330a1c
zkan <zkan.cs@gmail.com>
2023-12-09 10:47:22 +0700

    Test destroy tag with dependency

--- geeky/app/models/post.rb
+++ geeky/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :body, presence: true
   belongs_to :writer
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   include ContentEditable
--- geeky/test/models/tag_test.rb
+++ geeky/test/models/tag_test.rb
@@ -8,4 +8,10 @@ class TagTest < ActiveSupport::TestCase
   def test_has_many_posts
     assert_equal 1, @one.posts.count
   end
+
+  def test_tag_destroy
+    destroy = @one.destroy
+
+    assert_equal @one, destroy
+  end
 end
