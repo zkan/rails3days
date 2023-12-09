f1deff25860cf43d614f9f751d1ff537a9048c49
zkan <zkan.cs@gmail.com>
2023-12-09 13:39:39 +0700

    Show images from asset

---
+++ geeky/app/controllers/samples_controller.rb
@@ -0,0 +1,4 @@
+class SamplesController < ApplicationController
+  def show
+  end
+end
--- geeky/config/routes.rb
+++ geeky/config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   
   # Defines the root path route ("/")
   root "posts#index"
+
+  # get "samples/show"
+  resources :samples, only: ["index"]
 end
