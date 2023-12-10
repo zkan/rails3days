4d63ec7f75e8b757557e2368332d72cbede54739
zkan <zkan.cs@gmail.com>
2023-12-10 12:10:06 +0700

    Add custom JS

--- geeky/app/javascript/controllers/post_notice_controller.js
+++ geeky/app/javascript/controllers/post_notice_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 import Toastify from "toastify-js"
+import { hello } from "../utils/console"
 
 export default class extends Controller {
   connect() {
     console.log("connected")
+
+    hello()
 
     Toastify({
       text: "This is a toast",
